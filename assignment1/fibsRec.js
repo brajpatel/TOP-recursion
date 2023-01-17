357a073fa77789fd75ab740c6f6683e387c8c7eb
Brajesh <96828206+brajpatel@users.noreply.github.com>
2023-01-17 09:34:14 +0000

    Add "test cases"

--- assignment1/fibsRec.js
+++ assignment1/fibsRec.js
@@ -6,3 +6,10 @@ function fibsRec(n) {
 
     // return (n == 1) ? [0] : (n === 2) ? [0, 1] : [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]] ====== ONE LINER
 }
+
+console.log(fibsRec(-2)); // returns "[]"
+console.log(fibsRec(1)); // returns [0]
+console.log(fibsRec(2)); // returns [0, 1]
+console.log(fibsRec(5)); // returns [0, 1, 1, 2, 3]
+console.log(fibsRec(8)); // returns [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibsRec(12)); // returns [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
