f58a602a7b67b049a9e540e0fc5f6f914f4679e7
Brajesh <96828206+brajpatel@users.noreply.github.com>
2023-01-17 09:39:44 +0000

    Add "test cases"

--- assignment2/mergesort.js
+++ assignment2/mergesort.js
@@ -20,3 +20,8 @@ function merge(left, right) {
 
     return mergedArr.concat(left, right);
 }
+
+console.log(mergeSort([])) // returns []
+console.log(mergeSort([2,5,3,8,7,1,6,4])); // returns [1,2,3,4,5,6,7,8]
+console.log(mergeSort([40,20,10,80,30,50,70,60])); // returns [10,20,30,40,50,60,70,80]
+console.log(mergeSort([1,27,8,88,99999,2,69,1000])); // returns [1,2,8,27,69,88,1000,99999]
