87ad9591097c4df7a1f81719951bb128b16f22e3
Brajesh <96828206+brajpatel@users.noreply.github.com>
2023-01-17 09:53:24 +0000

    Update fibsRec.js

--- assignment1/fibsRec.js
+++ assignment1/fibsRec.js
@@ -1,6 +1,6 @@
 function fibsRec(n) {
     if(n === 1) return [0];
-    if(n === 2) return [0];
+    if(n === 2) return [0, 1];
 
     return (n <=1) ? n : [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]]
 
